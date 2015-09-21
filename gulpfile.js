59248629c2915e630e36b0feffa517ec5827ed1b
eisneim <eisneim1@gmail.com>
2015-09-21 23:05:26 +0800

    [done] gulp build for server and browser and AMD

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp'),fs = require("fs");
+
+var rename = require("gulp-rename");
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+// es6 related things
+var babel = require('gulp-babel');
+var babelify = require("babelify");
+var gbrowserify = require("gulp-browserify")
+
+gulp.task('default',['es6']);
+gulp.task("build",["test","es6","build-browser"]);
+
+gulp.task("es6",function(){
+	gulp.src("src/**/*.js")
+	.pipe( babel({
+		blacklist: ['regenerator','bluebirdCoroutines'],
+		// optional: ["es7.asyncFunctions"],
+		// only: './src/**/*.js',
+	}) )
+	.on( "error", handleError)
+	.pipe(gulp.dest("./"))
+})
+
+gulp.task("build-browser",function(){
+	gulp.src("src/validate-chain.js")
+	.pipe(gbrowserify({
+		// debug: argv.env !== 'production',
+		// insertGlobals : true,
+		transform: [
+			babelify.configure({
+			  // optional: ["es7.asyncFunctions","runtime"]
+			})
+		],
+	}))
+	.pipe( uglify({ compress:true, mangle:true}) )
+	.on( "error", handleError)
+	.pipe(rename({
+    suffix: "-browser.min",
+  }))
+	.pipe(gulp.dest("./"))
+})
+
+gulp.task("browser",function(){
+	gulp.src("src/validate-chain.js")
+		.pipe(gbrowserify({
+			// debug: argv.env !== 'production',
+			// insertGlobals : true,
+			transform: [
+				babelify.configure({
+				  // optional: ["es7.asyncFunctions","runtime"]
+				})
+			],
+		}))
+		.on( "error", handleError)
+		.pipe(rename({
+	    suffix: "-browser",
+	  }))
+		.pipe(gulp.dest("./"))
+})
+
+// gulp.task("watch-src",function(){
+// 	gulp.watch([paths.jsServer,paths.jsShared], function ( event ) {
+// 		console.log("src file changed >> "+ event.path.replace(__dirname,""))
+// 		var destPath ="./dist/" + event.path.replace(__dirname+"/src/","").replace(/[^\/]+$/,"");
+// 		console.log(destPath)
+		
+// 		return gulp.src( event.path )
+//       .pipe( babel({
+//       	blacklist: ['regenerator','bluebirdCoroutines'],
+//       	optional: ["es7.asyncFunctions"], // ,"runtime"
+//       	only: __dirname+'/src/**/*.js',
+//       }) )
+//       .on( "error", handleError)
+//       .pipe(gulp.dest( destPath))
+//     	.on('end', function() {
+//          fs.createWriteStream("./dist/.trigger");
+//       });
+// 	});
+
+// })
+
+
+// ---------------------- test ----------------
+
+var mocha = require('gulp-mocha');
+gulp.task('test_handle_err',function(){
+    gulp.src('./tests/**/*.spec.js', {read: false})
+      .pipe(mocha({
+      	reporter: 'spec',
+      	// compilers: 'js:babel/register'
+    }))
+    .on( "error", handleError); // spec,dot, nyan,list,doc,min,Progress, 
+})
+
+gulp.task('test',function(){
+    gulp.src('./tests/**/*.spec.js', {read: false})
+      .pipe(mocha({
+      	reporter: 'spec',
+    }))
+})
+
+gulp.task('watch-test',function(){
+  gulp.watch('./tests/**/*.spec.js', ['test_handle_err']);
+})
+
+// --------- utils ------------
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
---
+++ src/validate-chain.js
@@ -0,0 +1,292 @@
+/**
+ * validator util to make form validation chainable for both serverside and clientside
+ */
+(function (name, definition) {
+    if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+        module.exports = definition();
+    } else if (typeof define === 'function' && typeof define.amd === 'object') {
+        define(definition);
+    } else {
+        this[name] = definition();
+    }
+})('VC', function () {
+
+	var vv = require("validator");
+
+	var regx = {
+		phone: /^(\+?0?86\-?)?1[345789]\d{9}$/,
+		// email: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+		// creditCard: /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/,
+		objectId: /^[0-9a-fA-F]{24}$/,
+		alpha: /^[A-Z]+$/i,
+	  alphanumeric: /^[0-9A-Z]+$/i,
+	  numeric: /^[-+]?[0-9]+$/,
+	  int: /^(?:[-+]?(?:0|[1-9][0-9]*))$/,
+	  float: /^(?:[-+]?(?:[0-9]+))?(?:\.[0-9]*)?(?:[eE][\+\-]?(?:[0-9]+))?$/,
+	  hexadecimal: /^[0-9A-F]+$/i,
+	  hexcolor: /^#?([0-9A-F]{3}|[0-9A-F]{6})$/i, 
+		ascii: /^[\x00-\x7F]+$/,
+		base64: /^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i,
+	}
+
+	class Validator{
+		constructor( target, isUpdate ){
+			this.key = null; // temporarily store field name
+			this._errs = [];
+			this._san = {}; // sanitized object
+			this._alias = {}; // key: 中文名
+			this.opt = isUpdate? true: false;
+			this.target = target;
+		}
+		get errors(){
+			return this._errs
+		}
+		get sanitized(){
+			return this._san;
+		}
+		addError( msg ){
+			if(this._san[this.key]) delete this._san[this.key]
+
+			var alias = this._alias[this.key];	
+			if( alias ){
+				msg = alias+": "+ msg.replace(/\S+\:\s/,"")
+			}
+
+			this._errs.push( msg )
+			// this.next = false;
+		}
+		/**
+		 * set alias for a key and store them in a map: this._alias = {}
+		 * @param  {[type]} name [description]
+		 * @return {[type]}      [description]
+		 */
+		alias( name ){
+			if(!this.key) {
+				this.next = false; 
+				return this;
+			}
+			this._alias[ this.key ] = name;
+			return this;
+		}
+		// ----------------- start a validation chain with this method -----
+		check( key ){
+			this.key = key;
+			this.next = true;
+			this.opt = false;
+			if( this.target[ key ] !== undefined ){
+				this._san[key] = this.target[ key ];
+			} else{
+				this.opt = true;
+			}
+
+			return this
+		}
+		// ----------------- must in the beginning of the chain --------
+		required( tip ){
+			if( !this.next ) return this;
+			this.opt = false;
+			if( !this.target[ this.key ] ) {
+				this.addError( tip || `${this.key}: 为必填字段` )
+				this.next = false;
+			}
+			return this;
+		}
+		optional(){
+			if( !this.next ) return this;
+			this.opt = true;
+			return this;
+		}
+		// ----------------- property validate methods ------------------
+		between(min,max,tip){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+			
+			let type = typeof val;
+			if( type === "string" && (val.length > max || val.lenth< min)  ){
+				this.addError(tip || `${this.key}: 长度应该在${min}-${max}个字符之间`);
+			}else if( type === "number" && (val > max || val< min) ){
+				 this.addError(tip || `${this.key}: 大小应该在${min}-${max}之间`)
+			}
+			return this;
+		}
+		max(num,tip){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+			
+			let type = typeof val;
+			if( type === "string" && val.length > num ){
+				this.addError(tip || `${this.key}: 最多${num}个字符`); 
+			}else if( type === "number" && val > num ){
+				this.addError(tip || `${this.key}: 最大值为${num}`)
+			}
+			return this;
+		}
+		min(num,tip){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			let type = typeof val;
+			if( type === "string" && val.length < num ){
+				 this.addError(tip || `${this.key}: 最少${num}个字符`)
+			}else if( type === "number" && val < num ){
+				 this.addError(tip || `${this.key}: 最小值为${num}`)
+			}
+
+			return this;
+		}
+		regx( pattern, tip,modifiers){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+			if (Object.prototype.toString.call(pattern) !== '[object RegExp]') {
+	        pattern = new RegExp(pattern, modifiers);
+	    }
+			if( !pattern.test( val ) ){
+				this.addError( tip || `${this.key}: 不合格${pattern.toString()}的格式` )	
+			}
+
+			return this;
+		}
+		date( tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isDate( val ) ){
+				this.addError( tip || `${this.key}: ${val}不符合日期格式` )	
+			}
+
+			return this;
+		}
+		before( time, tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( vv.isBefore(val, time ) ){
+				this.addError( tip || `${this.key}: ${val}需要在${time}之前` )	
+			}
+
+			return this;
+		}
+		after( time, tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isAfter(val ,time  ) ){
+				this.addError( tip || `${this.key}: ${val}需要在${time}之后` )	
+			}
+			return this;
+		}
+		in( values,tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isIn( val, values ) ){
+				this.addError( tip || `${this.key}: ${val}需要在[${values.toString()}]之中` )	
+			}
+			return this;
+		}
+		email( tip, options ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isEmail( val, options ) ){
+				this.addError( tip || `${this.key}: ${val}不是常规的email` )	
+			}
+
+			return this;
+		}
+		JSON( tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isJSON( val ) ){
+				this.addError( tip || `${this.key}: ${val}不是JSON格式字符串` )	
+			}
+			return this;
+		}
+		URL( tip,options ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isURL( val,options) ){
+				this.addError( tip || `${this.key}: ${val}不符合URL的格式` )	
+			}
+			return this;
+		}
+		phone( tip ){
+			return this.regx( regx.phone , tip || `${this.key}: ${this.target[this.key]}不是常规的手机号码` );
+		}
+		numeric(tip){
+			return this.regx( regx.numeric, tip || `${this.key}: ${this.target[this.key]}必须为纯数字` );
+		}
+		float(tip){
+			return this.regx( regx.numeric, tip || `${this.key}: ${this.target[this.key]}必须为float格式数字` );	
+		}
+		hex( tip ){
+			return this.regx( regx.hexadecimal, tip || `${this.key}: ${this.target[this.key]}必须为16进制数字` );
+		}
+		alpha(tip){
+			return this.regx( regx.alpha, tip || `${this.key}: ${this.target[this.key]}必须为纯字母` );
+		}
+		alphanumeric(tip){
+			return this.regx( regx.alphanumeric, tip || `${this.key}: ${this.target[this.key]}必须为纯字母和数字的组合` );
+		}
+		ascii(tip){
+			return this.regx( regx.ascii, tip || `${this.key}: ${this.target[this.key]}必须为符合规范的ASCII码` );
+		}
+		objectId( tip ){
+			return this.regx( regx.objectId , tip || `${this.target[this.key]}不是常规的ObjectId` );
+		}
+		base64(tip){
+			return this.regx( regx.base64, tip || `${this.key}: ${this.target[this.key]}必须为符合规范的Base64编码` );
+		}
+		creditCard(tip){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isCreditCard( val ) ){
+				this.addError( tip || `${this.key}: ${val}不符合信用卡的格式` )	
+			}
+			return this;
+		}
+		currency( options,tip ){
+			if( !this.next ) return this;
+			let val = this.target[this.key];
+			if( this.opt && !val ) return this;
+
+			if( !vv.isCreditCard( val ) ){
+				this.addError( tip || `${this.key}: ${val}不符合信用卡的格式` )	
+			}
+			return this;
+		}
+		
+		// Currency(tip, options)
+
+		// ----------------- sanitizers ---------------
+		trim(){
+
+		}
+
+
+
+	}// end of class
+	if(process.browser){
+		window.VC = Validator;
+		window.Validator = vv;
+	}
+
+	return Validator;
+	
+});
+	
